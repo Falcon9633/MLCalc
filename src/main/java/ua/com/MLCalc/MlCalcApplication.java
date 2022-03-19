01bc8b1f8caacf8f33c40c9eae8191af37b98670
Orest Dmyterko <beckupfalcon@gmail.com>
2022-03-19 17:44:24 +0200

    skeleton

---
+++ src/main/java/ua/com/MLCalc/MlCalcApplication.java
@@ -0,0 +1,13 @@
+package ua.com.MLCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/MLCalc/MlCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.MLCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
